777d28e8c4935c0494e7bfff5f616ecea61c7642
Sathvik Shetty <119489811+SathvikShetty1@users.noreply.github.com>
2024-06-23 19:30:21 +0530

    Candidate.py

--- Candidate.py
+++ Candidate.py
@@ -1,31 +1,30 @@
-import csv
+import pandas as pd
 
-with open("enjoysport.csv") as f:
-    csv_file = csv.reader(f)
-    data = list(csv_file)
+df=pd.read_csv('enjoysport.csv')
+data = list(df.values)
 
-    specific = data[1][:-1]
-    general = [['?' for i in range(len(specific))] for j in range(len(specific))]
- 
-    for i in data[1:]:
-        if i[-1] == "yes":
-            for j in range(len(specific)):
-                if i[j] != specific[j]:
-                    specific[j] = "?"
-                    general[j][j] = "?"
+specific = data[0][:-1]
+general = [['?' for i in range(len(specific))] for j in range(len(specific))]
 
-        elif i[-1] == "no":
-            for j in range(len(specific)):
-                if i[j] != specific[j]:
-                    general[j][j] = specific[j]
-                else:
-                    general[j][j] = "?"
-      
-    gh = [] 
-    for i in general:
-        for j in i:
-            if j != '?':
-                gh.append(i)
-                break
-    print("\nFinal Specific hypothesis:\n", specific)
-    print("\nFinal General hypothesis:\n", gh)
+for i in data:
+    if i[-1] == "yes":
+        for j in range(len(specific)):
+            if i[j] != specific[j]:
+                specific[j] = "?"
+                general[j][j] = "?"
+
+    elif i[-1] == "no":
+        for j in range(len(specific)):
+            if i[j] != specific[j]:
+                general[j][j] = specific[j]
+            else:
+                general[j][j] = "?"
+  
+gh = [] 
+for i in general:
+    for j in i:
+        if j != '?':
+            gh.append(i)
+            break
+print("\nFinal Specific hypothesis:\n", specific)
+print("\nFinal General hypothesis:\n", gh)
