07d8e97ded14b1f9ee2d264fed99e7865078da8b
Sathvik Shetty <119489811+SathvikShetty1@users.noreply.github.com>
2024-06-23 19:02:55 +0530

    Candidate.py

--- Candidate.py
+++ Candidate.py
@@ -1,36 +1,31 @@
-import numpy as np 
-import pandas as pd
+import csv
 
-data = pd.read_csv('enjoysport.csv')
-concepts = np.array(data.iloc[:,0:-1])
-target = np.array(data.iloc[:,-1])
+with open("enjoysport.csv") as f:
+    csv_file = csv.reader(f)
+    data = list(csv_file)
 
+    specific = data[1][:-1]
+    general = [['?' for i in range(len(specific))] for j in range(len(specific))]
+ 
+    for i in data[1:]:
+        if i[-1] == "yes":
+            for j in range(len(specific)):
+                if i[j] != specific[j]:
+                    specific[j] = "?"
+                    general[j][j] = "?"
 
-def learn(concepts, target): 
-    specific_h = concepts[0].copy()
-    general_h = [["?" for i in range(len(specific_h))] for i in range(len(specific_h))]
-
-    for i, h in enumerate(concepts):
-        if target[i] == "yes":
-            for x in range(len(specific_h)): 
-                if h[x]!= specific_h[x]:                    
-                    specific_h[x] ='?'                     
-                    general_h[x][x] ='?'
-                   
-        if target[i] == "no":            
-            for x in range(len(specific_h)): 
-                if h[x]!= specific_h[x]:                    
-                    general_h[x][x] = specific_h[x]                
-                else:                    
-                    general_h[x][x] = '?'        
-        
-
-    indices = [i for i, val in enumerate(general_h) if val == ['?', '?', '?', '?', '?', '?']]    
-    for i in indices:   
-        general_h.remove(['?', '?', '?', '?', '?', '?']) 
-    return specific_h, general_h 
-
-s_final, g_final = learn(concepts, target)
-
-print(f"Final S:{s_final}")
-print(f"Final G:{g_final}")
+        elif i[-1] == "no":
+            for j in range(len(specific)):
+                if i[j] != specific[j]:
+                    general[j][j] = specific[j]
+                else:
+                    general[j][j] = "?"
+      
+    gh = [] 
+    for i in general:
+        for j in i:
+            if j != '?':
+                gh.append(i)
+                break
+    print("\nFinal Specific hypothesis:\n", specific)
+    print("\nFinal General hypothesis:\n", gh)
