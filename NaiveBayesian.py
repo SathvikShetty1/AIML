34459eb6bd45a95b737fed2e4b51d8a08c572e39
Sathvik Shetty <119489811+SathvikShetty1@users.noreply.github.com>
2024-06-24 14:18:34 +0530

    NaiveBayesian.py

---
+++ NaiveBayesian.py
@@ -0,0 +1,35 @@
+import pandas as pd
+msg = pd.read_csv('document.csv', names=['message', 'label'])
+print("Total Instances of Dataset: ", msg.shape[0])
+msg['labelnum'] = msg.label.map({'pos': 1, 'neg': 0})
+
+X = msg.message
+y = msg.labelnum
+print(X)
+print(y)
+
+from sklearn.model_selection import train_test_split
+Xtrain, Xtest, ytrain, ytest = train_test_split(X, y)
+print("\n Toatl no of training data:",ytrain.shape)
+print("\n Toatl no of test data:",ytest.shape)
+
+from sklearn.feature_extraction.text import CountVectorizer
+count_v = CountVectorizer()
+Xtrain_dm = count_v.fit_transform(Xtrain)
+Xtest_dm = count_v.transform(Xtest)
+print("\n the words or tokens in text document")
+print(count_v.get_feature_names())
+df = pd.DataFrame(Xtrain_dm.toarray(),columns=count_v.get_feature_names())
+
+
+from sklearn.naive_bayes import MultinomialNB
+clf = MultinomialNB()
+clf.fit(Xtrain_dm, ytrain)
+pred = clf.predict(Xtest_dm)
+
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score
+print('Accuracy Metrics: \n')
+print('Accuracy: ', accuracy_score(ytest, pred))
+print('Recall: ', recall_score(ytest, pred))
+print('Precision: ', precision_score(ytest, pred))
+print('Confusion Matrix: \n', confusion_matrix(ytest, pred))
