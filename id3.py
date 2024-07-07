ecf439d4eb4fb5b8719ad257f01e78bc42971010
Sathvik Shetty <119489811+SathvikShetty1@users.noreply.github.com>
2024-07-07 12:54:51 +0530

    id3.py

--- id3.py
+++ id3.py
@@ -1,97 +1,38 @@
-import math
 import pandas as pd
+import numpy as np
+from sklearn import metrics
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
 
-def infoGain(P, N):
-   
-    return -P / (P + N) * math.log2(P / (P + N)) - N / (P + N) * math.log2(N / (P + N))
+dataset = pd.read_csv('PlayTennis.csv')
+features = ['Outlook','Temperature','Humidity','Wind']
+X = dataset[features]
+Y = dataset.PlayTennis
 
-def insertNode(tree, addTo, Node):
-  
-    for k, v in tree.items():
-        if isinstance(v, dict):
-            tree[k] = insertNode(v, addTo, Node)
-    if addTo in tree:
-        if isinstance(tree[addTo], dict):
-            tree[addTo][Node] = 'None'
-        else:
-            tree[addTo] = {Node: 'None'}
-    return tree
+encoder = OneHotEncoder(sparse=False, handle_unknown='ignore')
+X_encoded = pd.DataFrame(encoder.fit_transform(X))
+columns = encoder.get_feature_names(features)
 
-def insertConcept(tree, addTo, Node):
-   
-    for k, v in tree.items():
-        if isinstance(v, dict):
-            tree[k] = insertConcept(v, addTo, Node)
-    if addTo in tree:
-        tree[addTo] = Node
-    return tree
+X_train,X_test,Y_train,Y_test = train_test_split(X_encoded, Y, test_size = 0.30, random_state = 100)
 
-def getNextNode(data, AttributeList, concept, conceptVals, tree, addTo):
-    
-    Total = data.shape[0]
-    if Total == 0:
-        return tree
-    
-    countC = {}
-    for cVal in conceptVals:
-        dataCC = data[data[concept] == cVal]
-        countC[cVal] = dataCC.shape[0]
-        
-    if countC[conceptVals[0]] == 0:
-        tree = insertConcept(tree, addTo, conceptVals[1])
-        return tree
-    
-    if countC[conceptVals[1]] == 0:
-        tree = insertConcept(tree, addTo, conceptVals[0])
-        return tree
-    
-    ClassEntropy = infoGain(countC[conceptVals[1]], countC[conceptVals[0]])
-    
-    Attr = {}
-    for a in AttributeList:
-        Attr[a] = list(set(data[a]))
-        
-    AttrCount = {}
-    EntropyAttr = {}
-    for att in Attr:
-        for vals in Attr[att]:
-            for c in conceptVals:
-                iData = data[data[att] == vals]
-                dataAtt = iData[iData[concept] == c]
-                AttrCount[c] = dataAtt.shape[0]
-            TotalInfo = AttrCount[conceptVals[1]] + AttrCount[conceptVals[0]]
-            if AttrCount[conceptVals[1]] == 0 or AttrCount[conceptVals[0]] == 0:
-                InfoGain = 0
-            else:
-                InfoGain = infoGain(AttrCount[conceptVals[1]], AttrCount[conceptVals[0]])
-                
-            if att not in EntropyAttr:
-                EntropyAttr[att] = (TotalInfo / Total) * InfoGain
-            else:
-                EntropyAttr[att] = EntropyAttr[att] + (TotalInfo / Total) * InfoGain
-                
-    Gain = {}
-    for g in EntropyAttr:
-        Gain[g] = ClassEntropy - EntropyAttr[g]
-        
-    Node = max(Gain, key=Gain.get)
-    
-    tree = insertNode(tree, addTo, Node)
-    for nD in Attr[Node]:
-        tree = insertNode(tree, Node, nD)
-        newData = data[data[Node] == nD].drop(Node, axis=1)
-        AttributeList = list(newData)[:-1]
-        tree = getNextNode(newData, AttributeList, concept, conceptVals, tree, nD)
-    return tree
+dtree = DecisionTreeClassifier(criterion = "entropy", random_state = 100)
+dtree.fit(X_train, Y_train)
+Y_pred = dtree.predict(X_test)
 
-def main():
-    data = pd.read_csv('PlayTennis.csv')
-    print(data)
-    AttributeList = list(data)[:-1]
-    concept = str(list(data)[-1])
-    conceptVals = list(set(data[concept]))
-    tree = getNextNode(data, AttributeList, concept, conceptVals, {'root': 'None'}, 'root')
-    print(tree)
+def classify_new_instance(outlook, temperature,humidity,wind,encoder):
+    instance = [[outlook, temperature,humidity,wind]]
+    instance_df = pd.DataFrame( instance, columns = features)
+    instance_encoded = encoder.transform(instance_df)
+    features_names = encoder.get_feature_names(features)
+    instance_encoded_df = pd.DataFrame(instance_encoded, columns = features_names)
+    prediction = dtree.predict(instance_encoded_df)
+    return prediction[0]
 
-if __name__ == "__main__":
-    main()
+pred = classify_new_instance( 'Rain', 'Mild', 'High', 'Strong', encoder)
+print("Prediction: ", pred)
+print("Accuracy: ", metrics.accuracy_score(Y_test, Y_pred))
+
+# OUTPUT:
+# Prediction:  No
+# Accuracy:  0.6
